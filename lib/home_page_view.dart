4fe9c18e104aabbdff93e4628ca172747eeba54c
syfulsharif <sharif.syful@gmail.com>
2023-07-13 01:53:16 +0600

    API Fetching successful

--- lib/home_page_view.dart
+++ lib/home_page_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:weatherapp/style.dart';
 import 'package:weatherapp/api_key.dart';
 import 'package:http/http.dart';
+import 'package:weatherapp/weatherData.dart';
 
 class HomePageView extends StatefulWidget {
   const HomePageView({super.key});
@@ -13,10 +14,13 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewState extends State<HomePageView> {
+  Map<String, dynamic> fetchedData = {};
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getWeatherData();
+    // print(fetchedData);
   }
 
   Future<void> getWeatherData() async {
@@ -25,8 +29,15 @@ class _HomePageViewState extends State<HomePageView> {
           'https://api.openweathermap.org/data/2.5/weather?q=Chittagong&appid=$myApiKey&units=metric'),
     );
     final Map<String, dynamic> decodedWeatherData = jsonDecode(response.body);
-    print(decodedWeatherData);
-
+    if (response.statusCode == 200) {
+      fetchedData.addAll(decodedWeatherData);
+      // print(fetchedData['name']);
+    }
+    setState(() {});
+    // return decodedWeatherData;
+    // print(response.body);
+    // var weatherImageSrc = 'https://openweathermap.org/img/wn/${decodedWeatherData['weather'][0]['icon']}@2x.png';
+    // print(decodedWeatherData['weather'][0]['main']);
   }
 
   //API https://api.openweathermap.org/data/2.5/weather?q=Chittagong&appid=myAPI
@@ -65,34 +76,32 @@ class _HomePageViewState extends State<HomePageView> {
       body: Stack(
         children: [
           screenBackground(context),
-          Container(
+          Padding(
+            padding: const EdgeInsets.all(20.0),
             child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Column(
-                  children: [
-                    Text('Chittagong'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.network(
-                            'https://openweathermap.org/img/wn/10d@2x.png'),
-                        Text('27' + '\u2103'),
-                        SizedBox(
-                          width: 25.0,
-                        ),
-                        Column(
-                          children: [
-                            Text('Max 25' + '\u2103'),
-                            Text('Min 25' + '\u2103')
-                          ],
-                        )
-                      ],
-                    ),
-                    Text('Thunder')
-                  ],
-                ),
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  Text(fetchedData['name']),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                          'https://openweathermap.org/img/wn/${fetchedData['weather'][0]['icon']}@2x.png'),
+                      Text('${fetchedData['main']['temp']}' ' ' '\u2103'),
+                      const SizedBox(
+                        width: 35.0,
+                      ),
+                      Column(
+                        children: [
+                          Text('Max' ' ''${fetchedData['main']['temp_max']}' ' '  '\u2103'),
+                          Text('Min' ' ''${fetchedData['main']['temp_min']}' ' '  '\u2103')
+                        ],
+                      )
+                    ],
+                  ),
+                  Text('${fetchedData['weather'][0]['main']}'),
+                ],
               ),
             ),
           )
